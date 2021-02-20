ce01f4eeb12be26050e724c27226a14a5301f752
BruceWnag-GQ <shuo.wang@beaconedu.com>
2021-02-20 17:50:46 +0800

    initialize the project

---
+++ src/main/java/com/ShuoWang/BeaconCommunity/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ShuoWang.BeaconCommunity;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
